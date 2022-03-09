fc61673519afc03b4efcdea6ed897185d0f5e5a4
Bekir Gulestan <gulestanbekir@gmail.com>
2022-03-09 19:36:36 +0300

    Edited Order and Uri in Connection for DB

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -27,12 +27,13 @@ app.use(errorHandler);
 
 const start = async () => {
   try {
-    await mongoose.connect("mongodb://auth-mongo-depl:27017");
-
-    app.listen(3000, () => {
-      console.log("listening on port 3000");
-    });
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
   } catch (err) {
     console.log(err);
   }
+
+  app.listen(3000, () => {
+    console.log("listening on port 3000");
+  });
 };
+start();
