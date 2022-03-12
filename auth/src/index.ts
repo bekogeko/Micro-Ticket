459149cd98c87dea1bb1e0b2bf91b659d6455611
Bekir Gulestan <gulestanbekir@gmail.com>
2022-03-12 18:53:26 +0300

    Setting Enviroment Variable to pods and use them

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -34,6 +34,10 @@ app.all("*", async () => {
 app.use(errorHandler);
 
 const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be provided");
+  }
+
   try {
     await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
   } catch (err) {
