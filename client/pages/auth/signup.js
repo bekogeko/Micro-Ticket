8a9a9d83463c3ca5f490caccfc2f54e71a252c08
Bekir Gulestan <gulestanbekir@gmail.com>
2022-03-17 20:58:29 +0300

    Signup Implemented And handling added

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,11 +1,24 @@
 import { useState } from "react";
+import axios from "axios";
 
 export default () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
 
-  const onSubmit = (event) => {
+  const onSubmit = async (event) => {
     event.preventDefault();
+    try {
+      const response = await axios.post("/api/users/signup", {
+        email,
+        password,
+      });
+
+      console.log(response.data);
+    } catch (err) {
+      console.log(err.response.data);
+      setErrors(err.response.data.errors);
+    }
   };
 
   return (
@@ -16,7 +29,6 @@ export default () => {
         <input
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-          type="email"
           className="form-control"
         />
       </div>
@@ -30,6 +42,16 @@ export default () => {
         />
       </div>
 
+      {errors.length > 0 && (
+        <div className="alert alert-danger">
+          <h4>Ooops.....</h4>
+          <ul className="my-0">
+            {errors.map((err) => (
+              <li key={err.message}> {err.message}</li>
+            ))}
+          </ul>
+        </div>
+      )}
       <button className="btn btn-primary"> Sign Up</button>
     </form>
   );
