a5e2e35db4edfc4b46f71f66bdea88c1a8ecd19d
Bekir Gulestan <gulestanbekir@gmail.com>
2022-03-20 17:29:04 +0300

    Tickets service added to skaffold
    Tickets service added to skaffold and also added an enviroment variable for mongodb URI

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -5,9 +5,13 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be provided");
   }
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI must be provided");
+  }
 
   try {
-    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to Mongodb");
   } catch (err) {
     console.log(err);
   }
